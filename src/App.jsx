383b16ac4c5aaa2fb80d115f07f323ca0e78e966
maimuna03134 <mj9644212@gmail.com>
2026-03-05 14:08:30 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,118 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense, use, useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+// import Banner from "./Components/Banner";
+// import Footer from "./Components/Footer";
+
+// import CustomerTickets from "./Components/CustomerTickets";
+// import TaskStatus from "./Components/TaskStatus";
+// import ResolvedTask from "./Components/ResolvedTask";
+// import { toast, ToastContainer } from "react-toastify";
+// import Container from "./Components/Container";
+
+// const fetchCustomerData = async () => {
+//   const res = await fetch("/data.json");
+//   return res.json()
+// };
+
+// const customerDataPromise = fetchCustomerData();
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const cardData = use(customerDataPromise);
+  // console.log(cardData);
+  // const [cards, setCards] = useState(() =>
+  //   cardData.map((card) => ({ ...card, status: "Open" }))
+  // );
+  // const [inProgressCount, setInProgressCount] = useState(0);
+  // const [resolvedCount, setResolvedCount] = useState(0);
+
+  // const handleCardClick = (id) => {
+  //   let isClicked = false;
+  //   setCards((element) =>
+  //     element.map((card) => {
+
+
+  //       if (card.id === id && card.status === "Open") {
+  //         if (!isClicked) {
+  //           toast(`🔵 ${card.title} 👉 is now In-Progress 😊`);
+  //           setInProgressCount((element) => element + 1);
+  //           isClicked = true;
+  //         }
+  //         return { ...card, status: "In-Progress" };
+  //       }
+  //       return card;
+  //     })
+  //   );
+  // };
+
+  // const handleComplete = (id) => {
+  //   let isClicked = false;
+  //   setCards((element) =>
+  //     element.map((card) => {
+
+  //       if (card.id === id && card.status === "In-Progress") {
+  //         if (!isClicked) {
+  //           toast(`✔️ ${card.title} has been completed ☺️`);
+  //           setResolvedCount((element) => element + 1);
+  //           setInProgressCount((element) => element - 1);
+  //           isClicked = true;
+  //         }
+  //         return { ...card, status: "Resolved" };
+  //       }
+  //       return card;
+  //     })
+  //   );
+  // };
 
   return (
     <>
-     
-      <h1 className='text-4xl'>Vite + React</h1>
-      
+      {/* navbar */}
+      <Navbar/>
+
+      {/* banner */}
+      {/* <Banner
+        inProgressCount={inProgressCount}
+        resolvedCount={resolvedCount}
+      ></Banner> */}
+
+      {/* customer ticket + task status + resolved task */}
+
+      {/* <Container>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 my-10 px-4">
+          {cards.length > 0 && (
+            <div className="md:col-span-2">
+              <Suspense
+                fallback={
+                  <span className="loading loading-dots loading-xl"></span>
+                }
+              >
+                <CustomerTickets
+                  cards={cards}
+                  handleCardClick={handleCardClick}
+                ></CustomerTickets>
+              </Suspense>
+            </div>
+          )}
+
+          <div className="flex flex-col gap-6">
+            <TaskStatus
+              cards={cards.filter((c) => c.status === "In-Progress")}
+              handleComplete={handleComplete}
+            ></TaskStatus>
+
+            <ResolvedTask
+              cards={cards.filter((c) => c.status === "Resolved")}
+            ></ResolvedTask>
+          </div>
+        </div>
+      </Container> */}
+
+      {/* footer */}
+      {/* <Footer></Footer>
+
+      <ToastContainer /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import Container from "./Container";
+
+const Navbar = () => {
+    return (
+        <div className="bg-white  shadow border-b-[0.5px] border-gray-300">
+            <Container>
+                <div className="navbar">
+                    <div className="navbar-start">
+                        <div className="lg:hidden">
+                            <div className="dropdown">
+                                <div
+                                    tabIndex={0}
+                                    role="button"
+                                    className="btn btn-ghost focus:outline-none lg:hidden"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-5 w-5 text-black"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M4 6h16M4 12h8m-8 6h16"
+                                        />
+                                    </svg>
+                                </div>
+                                <ul
+                                    tabIndex={0}
+                                    className="menu menu-sm dropdown-content text-center rounded-box z-10 mt-3 w-40 shadow bg-white"
+                                >
+                                    <li>
+                                        <a href="">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="">FAQ</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Changelog</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Blog</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Download</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <a>
+                            <h1 className="md:text-xl text-sm font-bold space-x-1 sm:space-x-0">
+                                CS — Ticket System
+                            </h1>
+                        </a>
+                    </div>
+
+                    <div className="navbar-end">
+                        <div className="hidden sm:block">
+                            <ul className="menu-horizontal px-1 text-sm space-x-2">
+                                <li>
+                                    <a href="">Home</a>
+                                </li>
+                                <li>
+                                    <a href="">FAQ</a>
+                                </li>
+                                <li>
+                                    <a href="">Changelog</a>
+                                </li>
+                                <li>
+                                    <a href="">Blog</a>
+                                </li>
+                                <li>
+                                    <a href="">Download</a>
+                                </li>
+                                <li>
+                                    <a href="">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="ml-2">
+                            <button className="bg-[linear-gradient(125.07deg,#632EE3,#9F62F2_100%)] text-white px-4 py-2  rounded-lg hover:cursor-pointer">
+                                + New Ticket
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Navbar;
